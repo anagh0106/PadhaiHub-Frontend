e925c8f63f9d2c6a874ed2fbbbd15b0a56aa3538
Anagh Patel <anagh0106@gmail.com>
2025-07-11 13:04:49 +0530

    update

--- src/components/AdminClassManagement.jsx
+++ src/components/AdminClassManagement.jsx
@@ -40,7 +40,7 @@ const AdminClassManagement = () => {
     const getSubjectsNameAPI = async () => {
         try {
             const res = await axios.get(`${API}/class/getSubject`);
-            const subjectNames = res.data.subjects.map(sub => sub);
+            const subjectNames = res.data.Subjects;
             console.log(subjectNames);
             setsubjectHandler(subjectNames);
         } catch (error) {
