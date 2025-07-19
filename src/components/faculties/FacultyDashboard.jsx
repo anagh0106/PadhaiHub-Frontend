35bdcd1427144013023876df056da91c96e5f411
Anagh Patel <anagh0106@gmail.com>
2025-07-19 10:28:20 +0530

    changes

--- src/components/faculties/FacultyDashboard.jsx
+++ src/components/faculties/FacultyDashboard.jsx
@@ -118,7 +118,7 @@ const FacultyDashboard = () => {
                                     >
                                         <div className="space-y-2">
                                             <h3 className="text-lg font-semibold text-gray-800 flex items-center gap-2">
-                                                📖 {cls.subject}
+                                                📖 {cls.subject} {cls.date}
                                             </h3>
                                             <p className="text-sm text-gray-600">
                                                 🏫 <span className="font-medium">Room:</span> {cls.room}
